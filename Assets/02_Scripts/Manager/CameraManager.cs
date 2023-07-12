5f9f2f8a4f1abc7622e64575f92d8b50bb26eb84
david0305 <kmchman@naver.com>
2023-07-12 11:33:26 +0900

    코드정리

--- Assets/02_Scripts/Manager/CameraManager.cs
+++ Assets/02_Scripts/Manager/CameraManager.cs
@@ -9,8 +9,6 @@ using UnityEngine.EventSystems;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
-    [SerializeField] private float panFactor = 0.05f;
-    [SerializeField] private float panReduceFactor = 10f;
 
     [SerializeField] private float zoomSpeed = 0.02f;
     [SerializeField] private int dragSpeed = 20;
@@ -22,12 +20,8 @@ public class CameraManager : MonoBehaviour
 
     private readonly float maxZoomFactor = 50;
     private readonly float minZoomFactor = 3;
-    private readonly float clampZoomOffset = 2.0f;
 
     private float minimumMoveDistanceForItemMove = 0.2f;
-    private float oldZoom;
-    private bool pinchStarted;
-    private float oldPinchDist;
 
     private static Vector3 PositiveInfinityVector = new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
     private CancellationTokenSource cts = new CancellationTokenSource();
@@ -35,43 +29,26 @@ public class CameraManager : MonoBehaviour
     private void Start()
     {
         bool groundDragStarted = false;
-        bool itemDragStarted = false;
+        bool itemDragStarted = false;    
+        bool pinchStarted = false;
+        float oldPinchDist = 0f;
         Vector3 dragStartPos = Vector3.zero;
         Vector3 newPos = Vector3.zero;
-        //Vector3 newZoom = Vector3.one;
-        float newZoomf = 10f;
+        float newZoom = 10f;
+        Vector3 oldPos = newPos;
+        float oldZoom = newZoom;
 
         BaseObj selectedObj = default;
-        Vector3 tapItemStartPos = default;
-        bool isObjDragStarted = false;
 
         UniTaskAsyncEnumerable.EveryUpdate(PlayerLoopTiming.Update).ForEachAwaitAsync(async _ =>
         {
             UpdateOneTouch();
 
 #if UNITY_EDITOR
-            InputKeyboard();
+            InputEditor();
 #else
             UpdateTwoTouch();
 #endif
-
-            
-
-//#if UNITY_EDITOR
-//            PanCamera();
-//            UpdateZoom();
-//            UpdateBaseItemMove();
-//            UpdateBaseItemTap();
-//#else
-//            if (Input.touchCount == 1)
-//            {
-//                PanCamera();
-//                UpdateZoom();
-//            }
-//            else if (Input.touchCount == 2)
-//            {
-//            }
-//#endif
 
             void UpdateOneTouch()
             {
@@ -83,12 +60,13 @@ public class CameraManager : MonoBehaviour
                         BaseObj[] baseObj = hitObj.GetComponentsInParent<BaseObj>();
                         selectedObj = baseObj[0];
                     }
-                    else
+
+                    Vector3 groudHitPoint = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
+                    if (!groudHitPoint.Equals(PositiveInfinityVector))
                     {
-                        Vector3 groudHitPoint = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
-                        if (!groudHitPoint.Equals(PositiveInfinityVector))
+                        dragStartPos = groudHitPoint;
+                        if (selectedObj == default)
                         {
-                            dragStartPos = groudHitPoint;
                             groundDragStarted = true;
                         }
                     }
@@ -99,7 +77,18 @@ public class CameraManager : MonoBehaviour
                     if (selectedObj != default)
                     {
                         var groundPos = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
-                        selectedObj.transform.SetPosition(groundPos);
+                        
+                        if (!itemDragStarted)
+                        {
+                            if (Vector3.Distance(groundPos, dragStartPos) > minimumMoveDistanceForItemMove)
+                            {
+                                itemDragStarted = true;
+                            }
+                        }
+                        else 
+                        {
+                            selectedObj.transform.SetPosition(groundPos);
+                        }
                     }
                     else
                     {
@@ -117,7 +106,9 @@ public class CameraManager : MonoBehaviour
                 if (Input.GetMouseButtonUp(0))
                 {
                     groundDragStarted = false;
+                    itemDragStarted = false;
                     selectedObj = default;
+                    dragStartPos = PositiveInfinityVector;
                 }
             }
 
@@ -142,252 +133,33 @@ public class CameraManager : MonoBehaviour
                     else
                     {
                         float delta = oldPinchDist - pinchDist;
-                        newZoomf = mainCamera.orthographicSize + delta / 2;
+                        newZoom = mainCamera.orthographicSize + delta / 2;
                     }
                 }
             }
 
-            void InputKeyboard()
+            void InputEditor()
             {
                 float scrollAmount = Input.GetAxis("Mouse ScrollWheel") * 10;
                 if (scrollAmount != 0)
                 {
-                    newZoomf = newZoomf - scrollAmount;
-                }
-                newZoomf = Mathf.Clamp(newZoomf, minZoomFactor, maxZoomFactor);
-            }
-            void UpdateTouch()
-            {
-
-                if (!Input.GetMouseButtonUp(0))
-                    return;
-
-                if (Input.touchCount == 1)
-                {
-                    UpdateOneTouch();
-                }
-                else if (Input.touchCount == 2)
-                {
-
-                }
-                
-                // 원 터치
-
-                // 터치다운
-                // 아이템 선택
-                // 그라운드 선택
-
-                // 터치중
-                // 최초 선택이 아이템
-                // 최소 이동 거리를 넘었을 경우 
-                // 아이템 드래그 시작
-
-                // 최초 선택이 그라운드
-                // Pan Camera
-
-                // 터치 업
-                // 아이템 드래그일경우
-                // 타겟이 아이템인경우 롤백
-
-                // 드래그 플래그 초기화
-
-                // 투터치
-                // 터치중
-                // 카메라 줌
-            }
-
-            void TouchLogic()
-            { 
-
-                // UI인경우 리턴
-
-                // 원 터치
-                  
-                    // 터치다운
-                        // 아이템 선택
-                        // 그라운드 선택
-
-                    // 터치중
-                        // 최초 선택이 아이템
-                            // 최소 이동 거리를 넘었을 경우 
-                                // 아이템 드래그 시작
-
-                        // 최초 선택이 그라운드
-                            // Pan Camera
-
-                    // 터치 업
-                        // 아이템 드래그일경우
-                            // 타겟이 아이템인경우 롤백
-
-                        // 드래그 플래그 초기화
-
-                // 투터치
-                    // 터치중
-                        // 카메라 줌
-
-            }
-
-            void UpdateBaseItemTap()
-            {
-                if (!Input.GetMouseButtonUp(0))
-                    return;
-
-                if (groundDragStarted)
-                    return;
-
-                if (itemDragStarted)
-                    return;
-
-                if (this.IsUsingUI())
-                {
-                    return;
-                }
-
-                GameObject obj = TryGetRayCastHitObj(Input.mousePosition, GameConfig.ItemLayerMask);
-
-                if (obj != default)
-                {
-                    BaseObj[] baseObj = obj.GetComponentsInParent<BaseObj>();
-                    selectedObj = baseObj[0];
-
-                    // objEvent
-                }
-                else
-                {
-                    selectedObj = null;
+                    newZoom = newZoom - scrollAmount;
                 }
             }
-
-            void UpdateBaseItemMove()
-            {
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    tapItemStartPos = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
-                    isObjDragStarted = false;
-                }
-
-                if (selectedObj == null)
-                    return;
-
-                if (Input.GetMouseButton(0) && !tapItemStartPos.Equals(PositiveInfinityVector))
-                {
-                    var tapItemCurrPos = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
-
-                    if (!isObjDragStarted)
-                    {
-                        if (Vector3.Distance(tapItemStartPos, tapItemCurrPos) >= minimumMoveDistanceForItemMove)
-                        {
-                            isObjDragStarted = true;
-                            Debug.Log("DragStarted");
-                        }
-                    }
-
-                    if (isObjDragStarted)
-                    {
-                        selectedObj.transform.position = tapItemCurrPos;
-                        // Dragging
-                        Debug.Log("Dragging");
-                    }
-                }
-
-                if (Input.GetMouseButtonUp(0))
-                {
-                    tapItemStartPos = PositiveInfinityVector;
-                    isObjDragStarted = false;
-                }
-
-            }
-
-            void PanCamera()
-            {
-                if (selectedObj != null)
-                    return;
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Vector3 hitPoint = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
-                    if (!hitPoint.Equals(PositiveInfinityVector))
-                    {
-                        dragStartPos = hitPoint;
-                        groundDragStarted = true;
-                    }
-                }
-
-                if (groundDragStarted && Input.GetMouseButton(0))
-                {
-                    Vector3 hitPoint = TryGetRayCastHitPoint(Input.mousePosition, GameConfig.GroundLayerMask);
-                    if (!hitPoint.Equals(PositiveInfinityVector))
-                    {
-                        newPos = transform.position + dragStartPos - hitPoint;
-                    }
-                }
-
-                if (Input.GetMouseButtonUp(0))
-                {
-                    groundDragStarted = false;
-                }
-            }
-
-            void UpdateZoom()
-            {
-                newZoomf = mainCamera.orthographicSize;
-                //Editor
-                float scrollAmount = Input.GetAxis("Mouse ScrollWheel") * 5f;
-                if (scrollAmount != 0)
-                {
-                    newZoomf = newZoomf - scrollAmount;
-                }
-
-                if (Input.touchCount == 0 || Input.touchCount == 1)
-                {
-                    pinchStarted = false;
-                }
-
-                if (Input.touchCount == 2)
-                {
-                    var touchPoint0 = TryGetRayCastHitPoint(Input.GetTouch(0).position, GameConfig.GroundLayerMask);
-                    var touchPoint1 = TryGetRayCastHitPoint(Input.GetTouch(1).position, GameConfig.GroundLayerMask);
-                    float pinchDist = Vector3.Distance(touchPoint0, touchPoint1);
-
-                    if (!pinchStarted)
-                    {
-                        oldPinchDist = pinchDist;
-                        pinchStarted = true;
-                    }
-                    else
-                    {
-                        float delta = oldPinchDist - pinchDist;
-                        newZoomf = mainCamera.orthographicSize + delta / 2;
-                    }
-                }
-
-                newZoomf = Mathf.Clamp(newZoomf, minZoomFactor, maxZoomFactor);
-
-                if (newZoomf < minZoomFactor + clampZoomOffset)
-                {
-                    newZoomf = Mathf.Lerp(newZoomf, this.minZoomFactor + clampZoomOffset, Time.deltaTime * 2);
-
-                }
-                else if (newZoomf > maxZoomFactor - clampZoomOffset)
-                {
-                    newZoomf = Mathf.Lerp(newZoomf, this.maxZoomFactor - clampZoomOffset, Time.deltaTime * 2);
-                }
-
-                if (oldZoom != newZoomf)
-                {
-                    mainCamera.orthographicSize = newZoomf;
-                    oldZoom = newZoomf;
-                }
-            }
-
         }, cts.Token).Forget();
         UniTaskAsyncEnumerable.EveryUpdate(PlayerLoopTiming.FixedUpdate).ForEachAwaitAsync(async _ =>
         {
             newPos = new Vector3(Mathf.Clamp(newPos.x, mapSizeMinX, mapSizeMaxX), Mathf.Clamp(newPos.y, 0, 0), Mathf.Clamp(newPos.z, mapSizeMinZ, mapSizeMaxZ));
-            newZoomf = Mathf.Clamp(newZoomf, minZoomFactor, maxZoomFactor);
-            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, newZoomf, Time.deltaTime * zoomSpeed);
-            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * dragSpeed);
+            newZoom = Mathf.Clamp(newZoom, minZoomFactor, maxZoomFactor);
+            if (!newPos.Equals(oldPos))
+            {
+                transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * dragSpeed);
+            }
+
+            if (!newZoom.Equals(oldZoom))
+            {
+                mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, newZoom, Time.deltaTime * zoomSpeed);
+            }
         }, cts.Token).Forget();
     }
 
@@ -456,4 +228,37 @@ public class CameraManager : MonoBehaviour
     //        //transform.position += delta;
     //    }
     //}
+
+
+    void TouchLogic()
+    {
+
+        // UI인경우 리턴
+
+        // 원 터치
+
+        // 터치다운
+        // 아이템 선택
+        // 그라운드 선택
+
+        // 터치중
+        // 최초 선택이 아이템
+        // 최소 이동 거리를 넘었을 경우 
+        // 아이템 드래그 시작
+
+        // 최초 선택이 그라운드
+        // Pan Camera
+
+        // 터치 업
+        // 아이템 드래그일경우
+        // 타겟이 아이템인경우 롤백
+
+        // 드래그 플래그 초기화
+
+        // 투터치
+        // 터치중
+        // 카메라 줌
+
+    }
+
 }
