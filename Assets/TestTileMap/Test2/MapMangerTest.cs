8ee234d774c6b511a66097d48f4e49d874c5878a
david0305 <kmchman@naver.com>
2023-08-10 17:01:13 +0900

    Added Test

--- Assets/TestTileMap/Test2/MapMangerTest.cs
+++ Assets/TestTileMap/Test2/MapMangerTest.cs
@@ -10,18 +10,29 @@ public class MapMangerTest : MonoBehaviour
     [SerializeField] private Tilemap tileMap;
     [SerializeField] private TileBase tilebase;
     //TileBase
+
+    private Vector3 prevPos;
     private void Update()
     {
 
         if (Input.GetMouseButtonDown(0))
         {
+            prevPos = testHeroObj.transform.position;
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var gridPos = tileMap.WorldToCell(worldPos);
-            
             var cellWorldPos = tileMap.GetCellCenterWorld(gridPos);
 
             testHeroObj.MoveTo(new Vector3(cellWorldPos.x, cellWorldPos.y, 2));
+            Debug.Log($"gridpos {gridPos}");
         }
+
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    var gridPos = tileMap.WorldToCell(prevPos);
+        //    var cellWorldPos = tileMap.GetCellCenterWorld(gridPos);
+        //    Debug.Log($"gridpos {gridPos}");
+        //    testHeroObj.MoveTo(new Vector3(cellWorldPos.x, cellWorldPos.y, 2));
+        //}
         
         
     }
