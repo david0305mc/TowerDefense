212d0988cb188a9c2fbf0db93ade8560a1e6ae0e
david0305 <kmchman@naver.com>
2023-08-17 17:09:14 +0900

    fixed Bug - enemyObj

--- Assets/02_Scripts/Game/Objects/MEnemyObj.cs
+++ Assets/02_Scripts/Game/Objects/MEnemyObj.cs
@@ -6,18 +6,25 @@ public class MEnemyObj : MBaseObj
 {
     public int UID { get; private set;}
     [SerializeField] private int tid;
+    [SerializeField] private UnityEngine.AI.NavMeshAgent agent;
+
     public int TID { get; }
 
     public DataManager.Character refData { get; set; }
 
     private System.Action getDamageAction;
-    
 
+    private void Awake()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
     public void InitObject(int _uid, System.Action _getDamageAction)
     {
         UID = _uid;
         getDamageAction = _getDamageAction;
         refData = DataManager.Instance.GetCharacterData(TID);
+        agent.isStopped = true;
     }
 
     public override void GetDamaged(int _damage)
