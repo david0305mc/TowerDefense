2d520ae2d65482eb08e5bf5c8d6533832640ba9d
david0305 <kmchman@naver.com>
2023-08-21 17:54:25 +0900

    Unused Code

--- Assets/02_Scripts/Manager/MGameManager.cs
+++ Assets/02_Scripts/Manager/MGameManager.cs
@@ -184,18 +184,18 @@ public class MGameManager : SingletonMono<MGameManager>
         InitEnemies();   
     }
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int gridPosition = currStageObj.tileMap.WorldToCell(mousePosition);
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        Vector3Int gridPosition = currStageObj.tileMap.WorldToCell(mousePosition);
 
-            TileBase clickedTile = currStageObj.tileMap.GetTile(gridPosition);
-            float walkingSpeed = dataFromTileMap[clickedTile].walkingSpeed;
-            Debug.Log($"walkingSpeed {walkingSpeed}");
-        }
-    }
+    //        TileBase clickedTile = currStageObj.tileMap.GetTile(gridPosition);
+    //        float walkingSpeed = dataFromTileMap[clickedTile].walkingSpeed;
+    //        Debug.Log($"walkingSpeed {walkingSpeed}");
+    //    }
+    //}
 
     public float GetTileWalkingSpeed(Vector3 worldPosition) 
     {
