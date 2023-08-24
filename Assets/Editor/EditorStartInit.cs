979ec637acd69efc629743c07c3a5539cb81e1cc
david0305 <kmchman@naver.com>
2023-08-24 11:14:57 +0900

    Removed EditStart

--- Assets/Editor/EditorStartInit.cs
+++ Assets/Editor/EditorStartInit.cs
@@ -3,13 +3,13 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
-[InitializeOnLoad]
-public class EditorStartInit
-{
-    static EditorStartInit()
-    {
-        var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
-        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
-        EditorSceneManager.playModeStartScene = sceneAsset;
-    }
-}
+//[InitializeOnLoad]
+//public class EditorStartInit
+//{
+//    static EditorStartInit()
+//    {
+//        var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
+//        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
+//        EditorSceneManager.playModeStartScene = sceneAsset;
+//    }
+//}
