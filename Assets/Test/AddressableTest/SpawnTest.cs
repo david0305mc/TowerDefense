75f585379e833fb94dd099110483f1af44d7696e
david0305 <kmchman@naver.com>
2023-08-31 21:29:46 +0900

    Addresssable Test

--- Assets/Test/AddressableTest/SpawnTest.cs
+++ Assets/Test/AddressableTest/SpawnTest.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class SpawnTest : MonoBehaviour
 {
-    [SerializeField] private GameObject spawnPrefab;
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.T))
         {
-            Instantiate(spawnPrefab, new Vector3(0, -10, 0), Quaternion.identity, transform);
+            //Instantiate(spawnPrefab, new Vector3(0, -10, 0), Quaternion.identity, transform);
+            for (int i = 1; i < 5; i++)
+            {
+                var go = Addressables.InstantiateAsync($"AddressablePrefab0{i}.prefab", Vector3.zero, Quaternion.identity, transform);
+            }
+            
         }
+
     }
 }
