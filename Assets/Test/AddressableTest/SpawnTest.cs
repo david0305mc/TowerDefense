90e3b2bf535c5dbbe9da3ebed291a24c0b4796a0
david0305 <kmchman@naver.com>
2023-09-01 18:08:49 +0900

    Test Addressable

--- Assets/Test/AddressableTest/SpawnTest.cs
+++ Assets/Test/AddressableTest/SpawnTest.cs
@@ -2,19 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using System.Diagnostics;
+using Cysharp.Threading.Tasks;
 
 public class SpawnTest : MonoBehaviour
 {
+
+    private Dictionary<string, GameObject> prefabDic = new Dictionary<string, GameObject>();
     private void Update()
     {
-        if (Input.GetKey(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.L))
         {
-            //Instantiate(spawnPrefab, new Vector3(0, -10, 0), Quaternion.identity, transform);
-            for (int i = 1; i < 5; i++)
+            //for (int i = 1; i < 6; i++)
+            //{
+            //    string name = $"Unit/AddressablePrefab0{i}.prefab";
+            //    prefabDic[name] = Addressables.LoadAssetAsync<GameObject>(name).WaitForCompletion();
+            //    //var go = Addressables.InstantiateAsync($"Unit/AddressablePrefab0{i}.prefab", Vector3.zero, Quaternion.identity, transform);
+            //}
+
+            for (int j = 0; j < 1000; j++)
             {
-                var go = Addressables.InstantiateAsync($"AddressablePrefab0{i}.prefab", Vector3.zero, Quaternion.identity, transform);
+                for (int i = 1; i < 6; i++)
+                {
+                    string name = $"Unit/AddressablePrefab0{i}.prefab";
+                    //prefabDic[name] = Addressables.LoadAssetAsync<GameObject>(name).WaitForCompletion();
+                    var go = Addressables.InstantiateAsync(name, new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0), Quaternion.identity, transform);
+                }
             }
-            
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                string name = $"Unit/AddressablePrefab0{i}.prefab";
+                prefabDic[name] = Addressables.LoadAssetAsync<GameObject>(name).WaitForCompletion();
+                //var go = Addressables.InstantiateAsync($"Unit/AddressablePrefab0{i}.prefab", Vector3.zero, Quaternion.identity, transform);
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                foreach (var item in prefabDic)
+                {
+                    Instantiate(item.Value, new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0), Quaternion.identity);
+                }
+            }
+            sw.Stop();
+            UnityEngine.Debug.Log($"Time : {sw.ElapsedMilliseconds}ms");
         }
 
     }
