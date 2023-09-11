3d3fe9680f481b894a62ba5bd1c84b6972a377ab
david0305 <kmchman@naver.com>
2023-09-11 18:45:44 +0900

    Added Default WorldMap Script

---
+++ Assets/02_Scripts/Game/WorldMap/WorldMapTileObj.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMapTileObj : MonoBehaviour
+{
+    [SerializeField] private int stageID;
+    public int StageID { get => stageID; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
