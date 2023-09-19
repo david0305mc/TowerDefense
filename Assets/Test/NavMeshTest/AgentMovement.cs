3fdd755225f9a3a2249671c7b5ff17e335f49d17
david0305 <kmchman@naver.com>
2023-09-19 15:08:03 +0900

    NavMesh Test

---
+++ Assets/Test/NavMeshTest/AgentMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovement : MonoBehaviour
+{
+    private Vector3 target;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        SetTargetPosition();
+        SetAgentPosition();
+    }
+
+    private void SetTargetPosition()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void SetAgentPosition()
+    {
+        agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
+    }
+}
