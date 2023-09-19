ab419c5ffefa310ed7a11ad61ab8b20d23d7c387
david0305 <kmchman@naver.com>
2023-09-19 19:36:17 +0900

    Attack Test

---
+++ Assets/Test/AttackTest/TestAttacker.cs
@@ -0,0 +1,122 @@
+using MonsterLove.StateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAttacker : MonoBehaviour
+{
+    public enum FSMStates
+    {
+        Idle,
+        Move,
+        Attack,
+    }
+
+    private Animator animator;
+    private UnityEngine.AI.NavMeshAgent agent;
+    public TestDefeneder target;
+    public float speed = 5f;
+    protected StateMachine<FSMStates, StateDriverUnity> fsm;
+    public string state;
+    private float timeElapse;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        fsm = new StateMachine<FSMStates, StateDriverUnity>(this);
+    }
+
+    private void Start()
+    {
+        StartFSM();
+    }
+
+    private void Update()
+    {
+        fsm.Driver.Update.Invoke();
+    }
+    public virtual void StartFSM()
+    {
+        fsm.ChangeState(FSMStates.Idle);
+    }
+
+    protected virtual void Idle_Enter()
+    {
+        PlayAni("Idle");
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+        agent.speed = 0f;
+        state = fsm.State.ToString();
+    }
+    protected virtual void Idle_Update()
+    {
+        if (Vector2.Distance(target.transform.position, transform.position) < 1f)
+        {
+            fsm.ChangeState(FSMStates.Attack);
+        }
+        else
+        {
+            fsm.ChangeState(FSMStates.Move);
+        }
+    }
+
+    protected virtual void Move_Enter()
+    {
+        PlayAni("Walk");
+        state = fsm.State.ToString();
+        agent.speed = speed;
+        agent.isStopped = false;
+    }
+    protected virtual void Move_Update()
+    {
+        agent.SetDestination(new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z));
+        FlipRenderers(agent.velocity.x < 0);
+        if (Vector2.Distance(target.transform.position, transform.position) < 1f)
+        {
+            fsm.ChangeState(FSMStates.Attack);
+        }
+    }
+
+    protected virtual void Attack_Enter()
+    {
+        PlayAni("Attack");
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+        agent.speed = 0f;
+        state = fsm.State.ToString();
+        timeElapse = 0f;
+    }
+    protected virtual void Attack_Update()
+    {
+        timeElapse += Time.deltaTime;
+        if (timeElapse >= 1f)
+        {
+            timeElapse = 0f;
+            PlayAni("Attack");
+            target.GetAttacked(gameObject);
+        }
+    }
+
+
+    public void PlayAni(string str)
+    {
+        animator.Play(str);
+        animator.Update(0);
+    }
+
+
+    protected void FlipRenderers(bool value)
+    {
+        if (value)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
---
+++ Assets/Test/AttackTest/TestDefeneder.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Cysharp.Threading.Tasks;
+
+public class TestDefeneder : MonoBehaviour
+{
+    
+    public GameObject target;
+    private NavMeshAgent agent;
+    private Rigidbody2D rigidBody2d;
+
+    private void Awake()
+    {
+        rigidBody2d = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        if (agent.isActiveAndEnabled)
+        {
+            if (Vector2.Distance(target.transform.position, transform.position) > 1f)
+            {
+                agent.SetDestination(new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z));
+                FlipRenderers(transform.position.x > target.transform.position.x);
+            }
+            else
+            {
+                agent.velocity = Vector3.zero;
+            }
+        }
+    }
+
+    protected void FlipRenderers(bool value)
+    {
+        if (value)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    public void GetAttacked(GameObject sender)
+    {
+        // Begin
+        agent.enabled = false;
+        Vector2 direction = (transform.position - sender.transform.position).normalized;
+        rigidBody2d.AddForce(direction * 8, ForceMode2D.Impulse);
+        // End
+        UniTask.Create(async () =>
+        {
+            await UniTask.Delay(100);
+            rigidBody2d.velocity = Vector3.zero;
+            agent.enabled = true;
+        });
+
+    }
+}
