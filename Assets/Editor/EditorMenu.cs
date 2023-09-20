4f2169b8c5d2eb652d3a96c58277feacc43a42b9
david0305 <kmchman@naver.com>
2023-09-20 16:44:42 +0900

    유저 데이터 제거 버튼

--- Assets/Editor/EditorMenu.cs
+++ Assets/Editor/EditorMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,5 +18,17 @@ public class EditorMenu
         DataManager.GenConfigTable();
         DataManager.GenTableEnum();
         Debug.Log("GenerateTableCode");
+    }
+
+    [MenuItem("Tools/ClearUserData")]
+    public static void ClearUserData()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            return;
+        }
+        string[] filePaths = Directory.GetFiles(Application.persistentDataPath);
+        foreach (string filePath in filePaths)
+            File.Delete(filePath);
     }
 }
