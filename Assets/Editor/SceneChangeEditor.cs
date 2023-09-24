336bfd963ab1ca25c9903f448380f0a61029e985
ejupjup <ejupjup@wemadeconnect.com>
2023-09-24 14:21:56 +0900

    더미 데이터 삭제 // 폭발 이펙트 임시 삭제

---
+++ Assets/Editor/SceneChangeEditor.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+
+public class SceneChangeEditor : Editor
+{
+    // MenuItem의 경로와 ditorSceneManager.OpenScene의 경로는 자신의 Scene 파일 이름으로 변경한다.
+    [MenuItem("SceneMove/Scene_Intro &1")]
+    private static void IntroScene()
+    {
+        EditorSceneManager.OpenScene("Assets/01_Scenes/Intro.unity");
+        Debug.Log("Move Intro Scene");
+    }
+
+    [MenuItem("SceneMove/Scene_Main &2")]
+    private static void MainScene()
+    {
+        EditorSceneManager.OpenScene("Assets/01_Scenes/Main.unity");
+        Debug.Log("Move Main Scene");
+    }
+
+    [MenuItem("SceneMove/Scene_UnitTest &3")]
+    private static void ThrowingBallScene()
+    {
+        EditorSceneManager.OpenScene("Assets/01_Scenes/UnitTest.unity");
+        Debug.Log("Move Unit Test");
+    }
+}
+#endif
