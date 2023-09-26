ce245256246fe5b879f99229deb015888ca4232e
david0305 <kmchman@naver.com>
2023-09-26 18:34:23 +0900

    UIStage 추가

---
+++ Assets/02_Scripts/UI/InGame/UIStage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UniRx;
+
+public class UIStage : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tileLeftText;
+    public void SetData(int time)
+    { 
+        //UniRx.Observable.Timer()
+    }
+}
