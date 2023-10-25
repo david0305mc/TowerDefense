c535d6a4fc2f0ceb0aa04d4b46cbc5abd0fa07d3
david0305 <kmchman@naver.com>
2023-10-25 12:49:46 +0900

    웨이브 스테이지 UI 추가

--- Assets/02_Scripts/UI/Main/UIChallengeCell.cs
+++ Assets/02_Scripts/UI/Main/UIChallengeCell.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIChallengeCell : MonoBehaviour
 {
     [SerializeField] Button startButton;
-         
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TextMeshProUGUI subTitleText;
+    [SerializeField] private List<UICell_Reward> cellRewardList;
+
     public void SetData(int _stageID, System.Action _startAction)
     {
+        var rewards = DataManager.Instance.GetStageRewards(_stageID);
+
+        for (int i = 0; i < rewards.Count; i++)
+        {
+            cellRewardList[i].SetActive(true);
+            cellRewardList[i].UpdateContent(new GridItemData(rewards[i].id));
+        }
+
+        for (int i = rewards.Count; i < cellRewardList.Count; i++)
+        {
+            cellRewardList[i].SetActive(false);
+        }
+        
         startButton.onClick.RemoveAllListeners();
         startButton.onClick.AddListener(() =>
         {
