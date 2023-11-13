7cd387a62dc659949a518bb5612dc27804d4e301
david0305 <kmchman@naver.com>
2023-11-14 08:20:54 +0900

    중력 가속도 테스트

---
+++ Assets/Test/MissileTest/GravityMissileTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class GravityMissileTest : MonoBehaviour
+{
+
+    public void Shoot(Vector3 endPos, float jumpSpeed, float gravity)
+    {
+        Vector3 start = transform.position; //현재 위치
+        Vector3 end = endPos; //끝나는 위치
+
+        float jumpTime = Mathf.Max(0.3f, Vector3.Distance(start, end) / jumpSpeed); //거리분의 속력을 해서 시간을 얻어냄 (거,속,시)
+        float currentTime = 0f;
+        float percent = 0f;
+        //y방향의 초기속도
+        float v0 = (end - start).y - gravity;
+
+
+        UniTask.Create(async () =>
+        {
+
+            while (percent < 1)
+            {
+                currentTime += Time.deltaTime;
+                percent = currentTime / jumpTime; //percent는 currentTime == jumpTime이 되어야 1이 된다
+
+                Vector3 position = Vector3.Lerp(start, end, percent); //자연스럽게 이동하기 위해 Lerp사용, percent만큼 이동
+
+                //포물선 운동 : 시작위치 + 초기속도*시간 + 중력*시간제곱
+                position.y = start.y + (v0 * percent) + (gravity * percent * percent);
+
+                transform.position = position;
+                await UniTask.Yield();
+            }
+        });
+    }
+}
--- Assets/Test/MissileTest/MissileTest.cs
+++ Assets/Test/MissileTest/MissileTest.cs
@@ -8,6 +8,7 @@ public class MissileTest : MonoBehaviour
     [SerializeField] private GameObject srcObj;
     [SerializeField] private GameObject dstObj;
     [SerializeField] private TestMissileObj01 missile01;
+    [SerializeField] private GravityMissileTest gravityMissile;
 
     [SerializeField] private Button button;
 
@@ -21,8 +22,11 @@ public class MissileTest : MonoBehaviour
 
     private void Shoot()
     {
-        var missile = Lean.Pool.LeanPool.Spawn(missile01, srcObj.transform);
-        missile.Shoot(dstObj, 3);
+        //var missile = Lean.Pool.LeanPool.Spawn(missile01, srcObj.transform);
+        //missile.Shoot(dstObj, 3);
+        var missile = Lean.Pool.LeanPool.Spawn(gravityMissile, srcObj.transform);
+        missile.Shoot(dstObj.transform.position, 3, -10);
     }
+
 
 }
