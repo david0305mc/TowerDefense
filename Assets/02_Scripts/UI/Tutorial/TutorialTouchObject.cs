f779f798be63ca87655ffb59baee6c6542aeebcb
david0305 <kmchman@naver.com>
2023-11-15 16:04:01 +0900

    튜토리얼 해상도 이슈 수정
    (cherry picked from commit 0d61c6f6d7440db67ec8ad1b6423edf45584e56b)

--- Assets/02_Scripts/UI/Tutorial/TutorialTouchObject.cs
+++ Assets/02_Scripts/UI/Tutorial/TutorialTouchObject.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using System.Threading;
 
 public class TutorialTouchObject : MonoBehaviour
 {
     [SerializeField] private GameObject arrowObj;
     [SerializeField] private Button nextButton;
+
+    private CancellationTokenSource cts;
+
+    private void OnDisable()
+    {
+        cts?.Cancel();
+        cts?.Dispose();
+        cts = null;
+    }
     public void SetData(int _tutoID, UnityEngine.Events.UnityAction _action)
     {
+        cts?.Cancel();
+        cts = new CancellationTokenSource();
+
         var tutoButtons = FindObjectsOfType<TutorialTouchEvent>();
-        var tutoEventButton = tutoButtons.FirstOrDefault(item => item.TutorialID == _tutoID);
+        TutorialTouchEvent tutoEventButton = tutoButtons.FirstOrDefault(item => item.TutorialID == _tutoID);
         if (tutoEventButton != null)
         {
-            arrowObj.transform.position = tutoEventButton.GetOffsetPosition();
+            arrowObj.transform.position = new Vector3(-3000, -3000, 0);
+
+            UniTask.Create(async () =>
+            {
+                while (true)
+                {
+                    await UniTask.Yield(cancellationToken: cts.Token);
+                    arrowObj.transform.position = tutoEventButton.GetOffsetPosition();
+                }
+            });
         }
         else
         {
