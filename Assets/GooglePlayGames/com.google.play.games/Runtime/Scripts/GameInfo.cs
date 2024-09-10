4c511a2c9f3fed1e9e6210fdaebe543f816a891a
david0305 <david0305mc@gmail.com>
2024-09-10 18:13:49 +0900

    앱 정보 임시 변경  To 어비스 클래식

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "123600794483"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "123600794483-2c1go8fqfgnftqn4udqoo1i3kosof95a.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
